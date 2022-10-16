96f58dd5aa154a974861a4a562c8ebf3341af32a
Yash-Kumar-Mittal <97656216+Yash-Kumar-Mittal@users.noreply.github.com>
2022-10-16 23:57:45 +0530

    Create Asg3.cpp

---
+++ Asg3.cpp
@@ -0,0 +1,47 @@
+//Yash Kumar Mittal 
+//SID:21105069
+
+
+#include <iostream>                     
+using namespace std;         
+
+/*
+
+QUESTION:How would you search for an element in an array/list whose size is unknown?
+
+SOLUTION: We can search an element in an array whose size is unknown in O(logn) time complexity provided that the array is in sorted in increasing order .We can do this by making some modification in binary search technique.
+
+The concept behind this approach is:
+  ->Take inital value of i as 0 and j as 1.
+  ->Now increase j to j*2 until arr[j] exceed the key to be searched.
+  ->Now search the key using binary search between i and j.
+
+*/
+
+//Simple Binary Search Function
+int binary_search(int arr[],int i,int j,int key){
+    int m=(i+j)/2; //m=middle element
+    while(i<=j){   //i=starting index j=ending index
+    if(arr[m]==key){
+      return m;
+    }  
+    else{
+        if(arr[m]<key){
+          i=m+1;
+        }
+        else{j=m-1;}
+    }
+    m=(i+j)/2;
+    }
+    return -1; 
+}
+//Enhanced binary search function or Binary search for array with unknown length
+int binary_search_unknown(int arr[],int key){
+    int i=0,j=1;
+    //We will increase j until arr[j] exceeds key to be searched.
+    while(arr[j]<key){
+        i=j;
+        j=2*j;
+    }
+    //Now calling Simple Binary Search on range i to j.
+    return binary_search(arr,i,j,key);
